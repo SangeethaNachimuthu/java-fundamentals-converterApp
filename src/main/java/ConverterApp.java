6b4b0a8743a21343e901a6771e64ce519c425863
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2025-12-03 12:08:57 +0100

    Updated ConverterApp file, added tempConverter logic

--- src/main/java/ConverterApp.java
+++ src/main/java/ConverterApp.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class ConverterApp {
 
     Scanner scan = new Scanner(System.in);
-    LocalDateTime dateTime = LocalDateTime.now();   //Get the current time
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-dd HH:mm");
 
     public void main(String[] args) {
 
@@ -14,11 +12,12 @@ public class ConverterApp {
         System.out.println("1. Length Converter");
         System.out.println("2. Weight Converter");
         System.out.println("3. Time Converter");
-        System.out.println("4. Exit");
+        System.out.println("4. Temperature Converter");
+        System.out.println("5. Exit");
 
         int option = 0;
         do {
-            System.out.println("Choose an option (1 to 4): ");
+            System.out.println("Choose an option (1 to 5): ");
             option = scan.nextInt();
             switch (option) {
                 case 1:
@@ -31,49 +30,55 @@ public class ConverterApp {
                     timeConverter();
                     break;
                 case 4:
+                    temperatureConverter();
+                case 5:
                     System.out.println("Thanks for using the Converter App.");
                     break;
                 default:
-                    System.out.println("Please enter the correct options from 1 to 4.");
+                    System.out.println("Please enter the correct options from 1 to 5.");
             }
-        } while (option != 4);
+        } while (option != 5);
     }
 
     /*
-    This method used to convert the meter into kilometer.
-    Using the LocalDateTime and formatter, displayed converted time.
+    This method used to convert the length from meter into kilometer.
      */
     public void lengthConverter() {
 
         double meter = getValidInput("Enter length in meter: ");
         double kilometer = meter/1000;
         System.out.println("Result: " +meter+ "m = " + kilometer +"km");
-
-        System.out.println("Converted at: " + dateTime.format(formatter));
+        convertedTime();
     }
 
     /*
-    This method used to convert the kilograms into grams.
-    Using the LocalDateTime and formatter, displayed converted time.
+    This method used to convert the weight from kilograms into grams.
      */
     public void weightConverter() {
         double kilograms = getValidInput("Enter weight in kilograms: ");
         double grams = kilograms*1000;
         System.out.println("Result: " +kilograms+ "kg = " + grams +"g");
-
-        System.out.println("Converted at: " + dateTime.format(formatter));
+        convertedTime();
     }
 
     /*
-    This method used to convert the hours into minutes.
-    Using the LocalDateTime and formatter, displayed converted time.
+    This method used to convert the time from hours into minutes.
      */
     public void timeConverter() {
         int hours = (int) getValidInput("Enter time in hours: ");
         int minutes = hours*60;
         System.out.println("Result: " +hours+ "hrs = " + minutes +"mins");
+        convertedTime();
+    }
 
-        System.out.println("Converted at: " + dateTime.format(formatter));
+    /*
+    This method used to convert the temperature from Celsius into Fahrenheit.
+     */
+    public void temperatureConverter() {
+        double tempInCel = getValidInput("Enter temperature in celsius: ");
+        double tempInFah = (tempInCel * 9/5) + 32;
+        System.out.println("Result: " +tempInCel+ "C = " + tempInFah +"F");
+        convertedTime();
     }
 
     /*
@@ -97,5 +102,14 @@ public class ConverterApp {
             }
         }
         return input;
+    }
+
+    /*
+     This method displays the current date and time in the formatted pattern.
+     */
+    public void convertedTime() {
+        LocalDateTime dateTime = LocalDateTime.now();   //Get the current time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-dd HH:mm");
+        System.out.println("Converted at: " + dateTime.format(formatter));
     }
 }
